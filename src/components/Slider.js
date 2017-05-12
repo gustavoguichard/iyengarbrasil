776f2cf93ae8856a10817c0b6ae5a509ae5c9fa0
Gustavo Guichard <gustavoguichard@gmail.com>
2017-05-12 19:28:36 -0300

    Finish slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import { floor } from 'lodash'
 
 export default class Slider {
   constructor(el, store) {
@@ -6,10 +7,44 @@ export default class Slider {
     this.store = store
 
     store.subscribe(this.update.bind(this), 'ticks')
+
+    this.mountSlider()
+  }
+
+  mountSlider() {
+    this.current = 0
+    this.container = $(this.el).find('.images-container')
+    this.time = this.el.dataset['time'] || 5
+
+    const images = $('.slider-img', this.container)
+    this.length = images.length
+
+    images.first().addClass('active')
+    $(this.container).after('<div class="slider-cover" />')
   }
 
   update() {
     const { ticks } = this.store.getState()
+    const newCurrent = floor(ticks / this.time) % this.length
+
+    if(this.current !== newCurrent) {
+      this.current = newCurrent
+      this.stepSlider(newCurrent)
+    }
+  }
+
+  stepSlider(newCurrent) {
+    const currEl = $('.active', this.container)
+    const nextEl = $('.slider-img', this.container).eq(newCurrent)
+    currEl
+      .removeClass('active')
+      .addClass('fading')
+      .fadeOut({
+        complete: () => currEl.removeClass('fading')
+      })
+    nextEl
+      .show()
+      .addClass('active')
   }
 
 }
