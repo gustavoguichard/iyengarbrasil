9ef60dffdc710437d1dc9ac224812fa797913009
Gustavo Guichard <gustavoguichard@gmail.com>
2017-05-15 16:02:55 -0300

    Adjusts in mobile

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,8 +1,10 @@
 import $ from 'jquery'
+import * as isMobile from '../utils/isMobile'
 
 export default class Parallax {
   constructor(el, store) {
     this.el = el
     $(this.el).prepend('<div class="parallax-cover" />')
+    isMobile.any() && $(this.el).addClass('mobile')
   }
 }
