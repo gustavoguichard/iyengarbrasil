86a60e136ab1cb846b2a6bc9935aba94effb286e
Gustavo Guichard <gustavoguichard@gmail.com>
2017-06-24 23:46:47 -0300

    Developed Slider + Parallax into one Vue component

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,15 +1,40 @@
-import $ from 'jquery'
+import Vue from 'vue'
+import { floor } from 'lodash'
 import isMobile from '../utils/isMobile'
+import MenuLink from '../vuecomponents/MenuLink'
 
-export default class Parallax {
-  static get selector() {
-    return '.sct-parallax'
-  }
-
-  constructor(el, store) {
-    this.el = $(el)
-    this.el.prepend('<div class="parallax-cover" />')
-
-    isMobile() && this.el.addClass('mobile')
+export default {
+  selector: '.sct-parallax',
+  vm: (el) => {
+    return new Vue({
+      el,
+      name: 'Parallax',
+      data: {
+        mobile: isMobile(),
+        length: 1,
+        current: 0,
+      },
+      methods: {
+        update: function({ ticks }) {
+          const time = this.$el.dataset.time || 5
+          const index = floor(ticks / time) % this.length
+          if(this.current !== index) {
+            this.current = index
+          }
+        }
+      },
+      computed: {
+        isSlider: function () {
+          return this.length > 1
+        },
+      },
+      mounted: function() {
+        this.length = this.$el.getElementsByClassName('slider-img').length
+        if(this.isSlider) {
+          this.$store.subscribe(this.update.bind(this), 'ticks')
+        }
+      },
+      components: { MenuLink },
+    })
   }
 }
