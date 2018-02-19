3c997bfc6dd32208a5901f08c5abe8b1370130e4
Gustavo Guichard <gustavoguichard@gmail.com>
2018-02-19 13:47:12 -0300

    Adjusting js menu for translation

--- functions.php
+++ functions.php
@@ -150,8 +150,7 @@ class StarterSite extends TimberSite {
 
   function add_to_context( $context ) {
     $is_english = get_locale() === 'en_US';
-    $menu_suffix = $is_english ? 'main-menu-en' : 'main-menu';
-    $menu_name = is_front_page() ? 'home-' . $menu_suffix : $menu_suffix;
+    $menu_name = $is_english ? 'main-menu-en' : 'main-menu';
     $context['menu'] = new TimberMenu($menu_name);
     $context['is_english'] = $is_english;
     $context['site'] = $this;
--- src/vuecomponents/MenuLink.js
+++ src/vuecomponents/MenuLink.js
@@ -2,14 +2,23 @@ import Vue from 'vue'
 import { mapState } from 'vuex'
 import { scrollToId } from '../utils/scroll'
 
-const template = `<a :href="href" :class="{ active: isActive }" @click="clicked">{{title}}</a>`
+const template = `<a :href="href" :class="{ active: isActive, 'is-home-page': isHomePage }" @click="clicked">{{title}}</a>`
 
 export default Vue.component('menu-link', {
   template,
   name: 'MenuLink',
-  props: ['href', 'title'],
+  props: ['href', 'title', 'is-home'],
+  mounted: function() {
+    if(this.isHomePage) {
+      this.url = this.$el.hash || this.$el.href
+      this.$el.href = this.url
+    }
+  },
   computed: {
     ...mapState(['currentSection']),
+    isHomePage() {
+      return !!parseInt(this.$props.isHome)
+    },
     isActive() {
       return this.currentSection === this.hash()
     },
@@ -20,7 +29,7 @@ export default Vue.component('menu-link', {
     },
     clicked(event) {
       this.$store.commit('closeMenu')
-      if(this.$props.href === this.$el.hash) {
+      if(this.url === this.$el.hash) {
         scrollToId(this.hash())
       }
     },
