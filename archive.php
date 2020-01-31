e7b9f262651632fc9bb9b9a16519a299d7d500fa
Gustavo Guichard <gustavoguichard@gmail.com>
2020-01-31 15:32:57 -0300

    Add archive-atividade.twig

--- archive.php
+++ archive.php
@@ -22,7 +22,8 @@ $context['title'] = 'Archive';
 if ( is_category() ) {
   $context['title'] = single_cat_title( '', false );
   $context['post'] = new TimberTerm(get_the_category()[0]);
-  array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
+  array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig', 'archive-' . get_post_type() . '.twig'
+);
 } else if ( is_post_type_archive() ) {
   $context['title'] = post_type_archive_title( '', false );
   array_unshift( $templates, 'archive-' . get_post_type() . '.twig' );
